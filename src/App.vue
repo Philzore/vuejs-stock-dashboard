557c60cb35cb506c93880a10fc8389d92d5b3ea4
Philipp Mössl <pmoessl@outlook.de>
2024-07-12 14:51:35 +0200

    add first component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
 <template>
-  <div id="app">
-
+  <div>
+    <BaseCard>
+      <h1>Title</h1>
+      <p>This is a paragraph inside the card.</p>
+    </BaseCard>
+    
   </div>
-  
+
 </template>
 
 <script>
-
+import BaseCard from './components/BaseCard.vue'
+import { stockService } from '@/services/stockService';
 
 export default {
   name: 'App',
   components: {
-    
+    BaseCard
+  },
+  async created() {
+    this.data = await stockService.getRevenue('$AAPL');
+    console.log('Loaded data', this.data);
   }
 }
 </script>
 
 <style>
 body {
+  
   margin: 0;
+  
+  
 }
+
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%) ;
+  padding: 150px;
   width: 100vw;
   min-height: 100vh;
-
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BaseCard'
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    background-color: #011F35;
+    padding: 20px 32px 20px 32px;
+    border-radius: 16px;
+  }
+  </style>
+  
